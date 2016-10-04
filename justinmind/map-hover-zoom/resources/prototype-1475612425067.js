4fd11a7c151b61293e46b209c2c385b0d14b2805
Stephen M <stephen@digidem.net>
2016-10-04 16:21:10 -0400

    jim pref updates and re-export

--- justinmind/map-hover-zoom/resources/prototype-1475612425067.js
+++ justinmind/map-hover-zoom/resources/prototype-1475612425067.js
@@ -1,4 +1,76 @@
-jQuery("#simulation")
+function initData() {
+  jimData.datamasters["globalSales"] = [
+    {
+      "id": 1,
+      "datamaster": "globalSales",
+      "userdata": {
+        "country": "United States",
+        "numberOfSales": "114",
+        "salesRevenue": "2408.95"
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "globalSales",
+      "userdata": {
+        "country": "Russia",
+        "numberOfSales": "199",
+        "salesRevenue": "4404.12"
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "globalSales",
+      "userdata": {
+        "country": "China",
+        "numberOfSales": "91",
+        "salesRevenue": "1866.52"
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "globalSales",
+      "userdata": {
+        "country": "Brazil",
+        "numberOfSales": "88",
+        "salesRevenue": "1208.71"
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "globalSales",
+      "userdata": {
+        "country": "Greenland",
+        "numberOfSales": "51",
+        "salesRevenue": "804.67"
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "globalSales",
+      "userdata": {
+        "country": "South Africa",
+        "numberOfSales": "73",
+        "salesRevenue": "994.23"
+      }
+    }
+  ];
+
+  jimData.datamasters["nationalSales"] = [
+    {
+      "id": 1,
+      "datamaster": "nationalSales",
+      "userdata": {
+        "country": "Greenland",
+        "company": "KNI A/S",
+        "numberOfSales": "324,951,876",
+        "salesRevenue": "1,314,916,000 (DKK)"
+      }
+    }
+  ];
+
+  jimData.isInitialized = true;
+}jQuery("#simulation")
   .on("click", ".s-d12245cc-1680-458d-89dd-4f0d7fb22724 .click", function(event, data) {
     var jEvent, jFirer, cases;
     if(data === undefined) { data = event; }
@@ -437,6 +509,99 @@ jQuery("#simulation")
     } else if(jFirer.is("#s-russia")) {
       jEvent.undoCases(jFirer);
     } else if(jFirer.is("#s-Image_2")) {
+      jEvent.undoCases(jFirer);
+    }
+  });(function(window, undefined) {
+  var dictionary = {
+    "d12245cc-1680-458d-89dd-4f0d7fb22724": "Screen 1",
+    "c8fc1480-80ef-4369-ba4f-f512c653073a": "dfsc",
+    "87db3cf7-6bd4-40c3-b29c-45680fb11462": "960 grid - 16 columns",
+    "e5f958a4-53ae-426e-8c05-2f7d8e00b762": "960 grid - 12 columns",
+    "f39803f7-df02-4169-93eb-7547fb8c961a": "Template 1",
+    "bb8abf58-f55e-472d-af05-a7d1bb0cc014": "default"
+  };
+
+  var uriRE = /^(\/#)?(screens|templates|masters|scenarios)\/(.*)(\.html)?/;
+  window.lookUpURL = function(fragment) {
+    var matches = uriRE.exec(fragment || "") || [],
+        folder = matches[2] || "",
+        canvas = matches[3] || "",
+        name, url;
+    if(dictionary.hasOwnProperty(canvas)) { /* search by name */
+      url = folder + "/" + canvas;
+    }
+    return url;
+  };
+
+  window.lookUpName = function(fragment) {
+    var matches = uriRE.exec(fragment || "") || [],
+        folder = matches[2] || "",
+        canvas = matches[3] || "",
+        name, canvasName;
+    if(dictionary.hasOwnProperty(canvas)) { /* search by name */
+      canvasName = dictionary[canvas];
+    }
+    return canvasName;
+  };
+})(window);jQuery("#simulation")
+  .on("mouseenter dragenter", ".s-c8fc1480-80ef-4369-ba4f-f512c653073a .mouseenter", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Image_2") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimSetValue",
+                  "parameter": {
+                    "target": "#s-nationalSales",
+                    "value": {
+                      "action": "jimFilterData",
+                      "parameter": {
+                        "datatype": "datamaster",
+                        "datamaster": "nationalSales",
+                        "value": {
+                          "action": "jimContains",
+                          "parameter": [ {
+                            "field": "company"
+                          },"KNI A/S" ]
+                        }
+                      }
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-nationalSales"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseleave dragleave", ".s-c8fc1480-80ef-4369-ba4f-f512c653073a .mouseleave", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Image_2")) {
       jEvent.undoCases(jFirer);
     }
   });
